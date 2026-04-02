f44f494958a44a5c94ea0e26955b0aa9548e207f
AlexDegerman <alex.degerman.dev@gmail.com>
2026-04-03 00:31:19 +0300

    fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,6 @@ const app = express()
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
