f3694f17dd281b47d94cf51a73407ac5970de0dc
AlexDegerman <alex.degerman.dev@gmail.com>
2026-04-08 01:05:26 +0300

    configure db

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -4,7 +4,10 @@ const { Pool } = pg
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 1,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 })
 
 export default pool
